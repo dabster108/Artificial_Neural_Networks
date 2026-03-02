bc6fde96aab2fa4d2d3720f63b25deb41ad16123
dabster108 <3108dikshanta@gmail.com>
2026-03-02 22:56:50 +0545

    sigmoid function with values created

--- ANN-Sigmoid_Function/sigmoid.py
+++ ANN-Sigmoid_Function/sigmoid.py
@@ -1,1 +1,26 @@
 #neural network activation function
+# Input - Layer 
+# Hidden - Layer 
+# Output - Layer 
+#inputs
+import numpy as np 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+
+x1 =2 
+x2 = 3 
+w1 = 0.5 
+w2 = -0.4 
+
+# Bias
+b = 0.1
+
+# Weighted sum
+z = (w1 * x1) + (w2 * x2) + b
+# Activation output
+a = sigmoid(z)
+
+print("z value:", z)
+print("Activated output:", a)
